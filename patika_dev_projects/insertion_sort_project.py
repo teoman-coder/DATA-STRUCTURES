45c9baf3f8e75aa9b6e3cf046f967582a08a7e43
teoman-coder <teomanates1@gmail.com>
2022-09-25 19:40:26 +0300

    init

--- patika_dev_projects/insertion_sort_project.py
+++ patika_dev_projects/insertion_sort_project.py
@@ -3,7 +3,7 @@
 
 1)Write the stages of the above given sequence according to the insertion sort.
 
-first-> [22|27,16,2,18,6] // first element sorted
+first-> [22|27,16,2,18,6] (first element sorted)
 
 """
 
